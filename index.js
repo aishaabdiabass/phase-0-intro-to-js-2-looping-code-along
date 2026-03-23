0ac9e42812b745b66b141c8f3d6848b1ca71753b
aisha abdi <aishaabdi1878@gmail.com>
2026-03-23 13:51:53 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+function countDown(start) {
+  let current = start;
+  while (current >= 0) {
+    console.log(current);
+    current--;
+  }
+}
+
+// Export functions for tests (if Node environment)
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { writeCards, countDown };
+}
